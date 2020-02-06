f2f5d3f07da6269a329703bba5627018fbb4c511
Jeremy Marino <jermarino16@gmail.com>
2020-02-06 07:26:29 -0800

    test program

---
+++ FirstProgram/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+
+	int favorite_number; 
+	std::cout << "Enter your favorite number between 1 and 100: " <<std::endl;
+	std::cin >>  favorite_number;
+
+	std::cout << "Your number is " << favorite_number << std::endl;
+	return 0;
+}
